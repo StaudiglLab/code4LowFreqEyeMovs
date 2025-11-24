e3cfa0d7dd84db9ccc614e876dd70142ddcff3ae
Xiongbo Wu <51327913+XiongboWu@users.noreply.github.com>
2025-11-24 15:53:12 +0100

    Add files via upload

--- TFR/README.txt
+++ TFR/README.txt
@@ -3,3 +3,4 @@ Time Frequency analyses for Experiment 1- 4
 ---------------
 
 TFR_Exp1.m: Time frequency analysis for Experiment 1 (same for Experiment 2 - 4)
+TFR_FOOOF_Exp1.m: Time frequency analysis using FOOOF-extracted periodic power for Experiment 1
--- TFR/TFR_FOOOF_Exp1.m
+++ TFR/TFR_FOOOF_Exp1.m
@@ -1,5 +1,4 @@
-%% Time Frequency analyis for Exp 3
-
+%% Time Frequency analyis for Exp 1
 codepath = '';
 addpath(genpath( [codepath,'\subfunctions\']))
 
@@ -7,58 +6,23 @@ addpath(genpath( [codepath,'\subfunctions\']))
 datapath = [codepath,'\datafiles\'];
 datafigspath =  [codepath,'\data4figs\'];
 figsavepath = [codepath,'\figures\'];
-subinfopath = [codepath,'\datafiles\Subjects_Exp3\ExpInfo\'];
-sacsavepath = [codepath,'\datafiles\Subjects_Exp3\saccades\'];
-eegsavepath = [codepath,'\datafiles\Subjects_Exp3\EEG\'];
-TFRsavepath = [codepath,'\datafiles\Subjects_Exp3\TFR\'];
+subinfopath = [codepath,'\datafiles\Subjects_Exp1\ExpInfo\'];
+sacsavepath = [codepath,'\datafiles\Subjects_Exp1\saccades\'];
+eegsavepath = [codepath,'\datafiles\Subjects_Exp1\EEG\'];
+TFRsavepath = [codepath,'\datafiles\Subjects_Exp1\TFR\'];
 % behavioral results
-savename = [datapath,'Beh_Exp3.mat'];
+savename = [datapath,'Beh_Exp1.mat'];
 load(savename)
 %%
-for s = 1:length(Mem)
-    
-    disp(s)
-    %% load EEG file of each subject
-    savename = [eegsavepath,sprintf('Sub_eegICA_PV_%d_%s.mat',s,'Exp3')];
-    load(savename);
-     
-    eeg = eeg_final;
-     
-    %% add beh
-    eeg.trialinfo(:,2) = Mem{s}(eeg.trialinfo(:,1));
-    eeg.trialinfo(:,3) = Confi{s}(eeg.trialinfo(:,1));
-
-    %% do time freq analysis
-    cfg              = [];
-    cfg.latency      = [-1.499,4.5];
-    eeg = ft_selectdata(cfg,eeg);
-    
-    
-    cfg              = [];
-    cfg.output       = 'pow';
-    cfg.method       = 'mtmconvol';
-    cfg.taper        = 'hanning';
-    cfg.foi          = 2:2:40;                         % default : analysis 2 to 30 Hz in steps of 2 Hz
-    cfg.t_ftimwin    = ones(length(cfg.foi),1).*0.5;   % default : length of time window = 0.5 sec 
-    cfg.toi          = -1:0.05:4;
-    cfg.keeptrials = 'yes';
-    tfralltrl = ft_freqanalysis(cfg, eeg);
-    
-    %% save data
-
-    savename = [TFRsavepath,sprintf('TFR_PVstim_allTrials_sub%d_%s.mat',s,'Exp3')];
-    save(savename,'tfralltrl','-v7.3')
-
-end
-%% Analysis
 rng(2025);
-for s = 1:length(Mem)
+
+for s = 1:20
     disp(s)
-    savename = [TFRsavepath,sprintf('TFR_PVstim_allTrials_sub%d_%s.mat',s,'Exp3')];
+    savename = [TFRsavepath,sprintf('TFR_PVstim_allTrials_sub%d_%s.mat',s,'Exp1')];
     load(savename,'tfralltrl')
     
     % load trial info
-    load([sacsavepath,sprintf('TrialEyeInfo_%d_%s.mat',s,'Exp3')])
+    load([sacsavepath,sprintf('TrialEyeInfo_%d_%s.mat',s,'Exp1')])
     %% select trials
     % select only the intersect between clean EEG and clean eye trials
     cfg = [];
@@ -83,22 +47,29 @@ for s = 1:length(Mem)
     % 
     tfr_C1_raw{s} = tfr_C1;
     tfr_C2_raw{s} = tfr_C2;
+
+    %
+    
+    % apply fooof
+    tfr_C1_raw{s} = TFR_fooof(tfr_C1);
+    tfr_C2_raw{s} = TFR_fooof(tfr_C2);
     
     
-   %% by median explore index
-    cri = nanmedian(tfrfinal.trialinfo(:,5));
+    %% by median N saccades
+    cri = nanmedian(tfrfinal.trialinfo(:,4));
     
     cfg= [];
-    cfg.trials = tfrfinal.trialinfo(:,5)<=cri;
-    n_trl_ExpIdx(s,1) = sum(cfg.trials);
-    tfr_MoreExpl_raw{s}  = ft_freqdescriptives(cfg, tfrfinal);
-    cfg.trials = tfrfinal.trialinfo(:,5)>=cri;
-    n_trl_ExpIdx(s,2) = sum(cfg.trials);
-    tfr_LessExpl_raw{s}  = ft_freqdescriptives(cfg,tfrfinal);
+    cfg.trials = tfrfinal.trialinfo(:,4)>=cri;
+    n_trl_Nsac(s,1) = sum(cfg.trials);
+    dat = ft_freqdescriptives(cfg, tfrfinal);
+    tfr_MoreNsac_raw{s}  =TFR_fooof(dat); clearvars dat
+    cfg.trials = tfrfinal.trialinfo(:,4)<=cri;
+    n_trl_Nsac(s,2) = sum(cfg.trials);
+    dat = ft_freqdescriptives(cfg, tfrfinal);
+    tfr_LessNsac_raw{s}  = TFR_fooof(dat); clearvars dat
     clearvars cri
-    
-
 end
+
 
 % baseline correction 
 baseline_t = [-1,-0.5];
@@ -106,19 +77,20 @@ baseline_t = [-1,-0.5];
 for s = 1:length(tfr_C1_raw)
 cfg = [];
     cfg.baseline =baseline_t;
-    cfg.baselinetype = 'db';
+    cfg.baselinetype = 'absolute';
     
     tfr_C1_ave{s} = ft_freqbaseline(cfg,tfr_C1_raw{s});
     tfr_C2_ave{s} = ft_freqbaseline(cfg,tfr_C2_raw{s});
     
-    tfr_Mexpl_ave{s} = ft_freqbaseline(cfg,tfr_MoreExpl_raw{s});
-    tfr_Lexpl_ave{s} = ft_freqbaseline(cfg,tfr_LessExpl_raw{s});
-   
- 
+    tfr_Msac_ave{s} = ft_freqbaseline(cfg,tfr_MoreNsac_raw{s});
+    tfr_Lsac_ave{s} = ft_freqbaseline(cfg,tfr_LessNsac_raw{s});
+    
 end
+
 %% statistics 
-rng(2025);
+rng(2015);
 load([datapath,'layout.mat'])
+
 subset = 1:20;
 design      = zeros(2,length(subset)*2);
 design(1,:) = repmat(1:length(subset),[1 2]);
@@ -151,13 +123,13 @@ cfg.design           = design;
 cfg.avgoverfreq = 'no';
 
 [stat_mem]  = ft_freqstatistics(cfg, tfr_C1_ave{subset},tfr_C2_ave{subset});
-[stat_expl] = ft_freqstatistics(cfg, tfr_Mexpl_ave{subset},tfr_Lexpl_ave{subset});
+[stat_sac]  = ft_freqstatistics(cfg, tfr_Msac_ave{subset},tfr_Lsac_ave{subset});
 
 %% save outputs
-stat_mem = rmfield(stat_mem,'cfg');% remove cfg to save space
-stat_expl = rmfield(stat_expl,'cfg');
-save([datafigspath,'Fig3C.mat'],'stat_mem')
-save([datafigspath,'Fig3E_left.mat'],'stat_expl')
+stat_mem = rmfield(stat_mem,'cfg');% save space
+stat_sac = rmfield(stat_sac,'cfg');
+save([datafigspath,'SuppleFig3_left.mat'],'stat_mem')
+save([datafigspath,'SuppleFig3_right.mat'],'stat_sac')
 
 
 %% Figure
@@ -168,9 +140,9 @@ datapath = [codepath,'\datafiles\'];
 datafigspath =  [codepath,'\data4figs\'];
 figsavepath = [codepath,'\figures\'];
 load([datapath,'layout.mat']) % load layout
-% load behavioral data, gaze data
-load( [datafigspath,'Fig3C.mat'])
-load( [datafigspath,'Fig3E_left.mat'])
+% load data
+load( [datafigspath,'SuppleFig3_left.mat'])
+load( [datafigspath,'SuppleFig3_right.mat'])
 
 
 plot_save = 1; % to save or not figure
@@ -179,26 +151,23 @@ plot_save = 1; % to save or not figure
 sizefig1 = [0.2,0.2,0.6,0.7];
 sizefig2 = [0.2,0.2,0.35,0.4];
 fontaxis = 24;line_width = 2;
-fig_savename1 = 'Fig3C1';
-fig_savename2 = 'Fig3C2';
-
-% no sig cluster for Sub memory, define a time window and frequecy for TFR
-% and Topo plotting
-toi   = [0.5,4];
-foi = [10,20];
+fig_savename1 = 'SuppleFig3_left1';
+fig_savename2 = 'SuppleFig3_left2';
 
 for remforg=1
-
+    sig_channel = sum(sum(stat_mem.mask,3),2)>0;
     stat_mem_ave.dimord = 'chan_freq_time';
     stat_mem_ave.time   = stat_mem.time;
     stat_mem_ave.freq   = stat_mem.freq;
     stat_mem_ave.maskTF =(sum(stat_mem.mask,1))>0;
-    stat_mem_ave.stat =mean(stat_mem.stat,1);
+    stat_mem_ave.stat =mean(stat_mem.stat(sig_channel,:,:),1);
     stat_mem_ave.label = {'average'};
     
     cfg = [];
     cfg.channel = {'average'};
     cfg.parameter = 'stat';
+    cfg.maskparameter = 'maskTF';
+    cfg.maskstyle        = 'outline';
     cfg.comment        = 'no';
     cfg.shading             = 'interp';
     cfg.zlim = [-2.5 2.5];
@@ -207,6 +176,7 @@ for remforg=1
     colormap(flipud(cbrewer2('RdBu','div')));
     title('TFR Rem vs Forg')
     ylabel('Frequency (Hz)')
+    xlabel('Time [Sec]')
     c = colorbar;
     c.Label.String = 't value';
     set(findobj(gcf,'type','axes'),'FontName','Arial','FontSize',fontaxis,'FontWeight','Bold', 'LineWidth', line_width);
@@ -218,8 +188,8 @@ for remforg=1
     
     % topoplot
     for chan=1:size(stat_mem.stat,1)
-        chan_t = stat_mem.stat(chan,dsearchn(stat_mem.freq',foi(1)):dsearchn(stat_mem.freq',foi(2)),dsearchn(stat_mem.time',toi(1)):dsearchn(stat_mem.time',toi(2)));
-        topo_t(chan,1) = mean(chan_t(:));
+        chan_t = stat_mem.stat(chan,:,:);
+        topo_t(chan,1) = mean(chan_t(sum(stat_mem.mask,1)>0));
     end
     
     topo_stat_mem.stat = topo_t;
@@ -229,7 +199,6 @@ for remforg=1
     
     cfg         = [];
     cfg.parameter = 'stat';
-    %cfg.maskparameter = 'mask';
     cfg.marker        = 'off';
     cfg.zlim = 'absmax';
     cfg.comment        = 'no';
@@ -237,6 +206,10 @@ for remforg=1
     cfg.layout  = lay;
     cfg.colormap = '*RdBu';
     
+    cfg.highlight          = 'on';
+    cfg.highlightchannel = {stat_mem.label{sig_channel}};
+    cfg.highlightsymbol   = '.';
+    cfg.highlightsize      = 10;
     cfg.comment            = 'no';
     cfg.shading             = 'interp';
     cfg.style               ='straight';
@@ -251,18 +224,17 @@ for remforg=1
     end
 end
 
+fig_savename1 = 'SuppleFig3_right1';
+fig_savename2 = 'SuppleFig3_right2';
 
-fig_savename1 = 'Fig3E_left1';
-fig_savename2 = 'Fig3E_left2';
-
-for EI=1
-    sig_channel = sum(sum(stat_expl.mask,3),2)>0;
-    stat_expl_ave.dimord = 'chan_freq_time';
-    stat_expl_ave.time   = stat_expl.time;
-    stat_expl_ave.freq   = stat_expl.freq;
-    stat_expl_ave.maskTF =(sum(stat_expl.mask,1))>0;
-    stat_expl_ave.stat =mean(stat_expl.stat(sig_channel,:,:),1);
-    stat_expl_ave.label = {'average'};
+for nsac=1
+    sig_channel = sum(sum(stat_sac.mask,3),2)>0;
+    stat_sac_ave.dimord = 'chan_freq_time';
+    stat_sac_ave.time   = stat_sac.time;
+    stat_sac_ave.freq   = stat_sac.freq;
+    stat_sac_ave.maskTF =(sum(stat_sac.mask,1))>0;
+    stat_sac_ave.stat =mean(stat_sac.stat(sig_channel,:,:),1);
+    stat_sac_ave.label = {'average'};
     
     cfg = [];
     cfg.channel = {'average'};
@@ -273,7 +245,7 @@ for EI=1
     cfg.shading             = 'interp';
     cfg.zlim = [-2.5 2.5];
     f=figure('Name',int2str(11),'units','normalized','outerposition',sizefig1);clf;
-    ft_singleplotTFR(cfg,stat_expl_ave);
+    ft_singleplotTFR(cfg,stat_sac_ave);
     colormap(flipud(cbrewer2('RdBu','div')));
     title('TFR More vs Less Saccades')
     ylabel('Frequency (Hz)')
@@ -288,15 +260,15 @@ for EI=1
     end
     
     % topoplot
-    for chan=1:size(stat_expl.stat,1)
-        chan_t = stat_expl.stat(chan,:,:);
-        topo_t(chan,1) = mean(chan_t(sum(stat_expl.mask,1)>0));
+    for chan=1:size(stat_sac.stat,1)
+        chan_t = stat_sac.stat(chan,:,:);
+        topo_t(chan,1) = mean(chan_t(sum(stat_sac.mask,1)>0));
     end
     
-    topo_stat_expl.stat = topo_t;
-    topo_stat_expl.dimord='chan_time';
-    topo_stat_expl.time=0;
-    topo_stat_expl.label=stat_expl.label;
+    topo_stat_sac.stat = topo_t;
+    topo_stat_sac.dimord='chan_time';
+    topo_stat_sac.time=0;
+    topo_stat_sac.label=stat_sac.label;
     
     cfg         = [];
     cfg.parameter = 'stat';
@@ -308,14 +280,14 @@ for EI=1
     cfg.colormap = '*RdBu';
     
     cfg.highlight          = 'on';
-    cfg.highlightchannel = {stat_expl.label{sig_channel}};
+    cfg.highlightchannel = {stat_sac.label{sig_channel}};
     cfg.highlightsymbol   = '.';
     cfg.highlightsize      = 10;
     cfg.comment            = 'no';
     cfg.shading             = 'interp';
     cfg.style               ='straight';
     f=figure('Name',int2str(12),'units','normalized','outerposition',sizefig2);clf;
-    ft_topoplotTFR(cfg,topo_stat_expl);
+    ft_topoplotTFR(cfg,topo_stat_sac);
     c = colorbar;
     c.Label.String = 't value';
     set(findobj(gcf,'type','axes'),'FontName','Arial','FontSize',fontaxis,'FontWeight','Bold', 'LineWidth', line_width);
@@ -324,3 +296,4 @@ for EI=1
         saveas(gcf,[figsavepath,fig_savename2,'.png'])
     end
 end
+
