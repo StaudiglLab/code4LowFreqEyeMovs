0b469c49ac96bc5d6338ef75f2aedbb5cfb562bd
Xiongbo Wu <51327913+XiongboWu@users.noreply.github.com>
2026-04-29 17:15:33 +0200

    Add files via upload

--- Gaze/GazeAnalysis_Exp1.m
+++ Gaze/GazeAnalysis_Exp1.m
@@ -220,11 +220,15 @@ for s = 1:length(ave_cl_gaze_RF_balance)
 end
 
 
+effect1 = computeCohen_d(MemMore_Sac(:,1),MemFewer_Sac(:,1),'paired');
+effect2 = computeCohen_d(MemMore_Sac(:,2),MemFewer_Sac(:,2),'paired');
+effect3 = computeCohen_d(MemMore_Sac(:,1),MemMore_Sac(:,2),'paired');
+effect4 = computeCohen_d(MemFewer_Sac(:,1),MemFewer_Sac(:,2),'paired');
 %% save output for plotting
 save([datafigspath,'Fig1B_left.mat'],'stat_CB')
 save([datafigspath,'Fig1D_left.mat'],'stat_Sac')
 save([datafigspath,'Fig1D_right.mat'],'MemMore_Sac','MemFewer_Sac','MemHigh_gaze_cl_balance','MemLow_gaze_cl_balance')
-save([datafigspath,'SuppleFig4_right11.mat'],'stat_EI','Gaze_MoreExplr','Gaze_LessExplr')
+save([datafigspath,'SuppleFig9_right11.mat'],'stat_EI','Gaze_MoreExplr','Gaze_LessExplr')
 %% Figure
 % load data for plotting
 codepath = '';
@@ -388,3 +392,293 @@ for fig=52
 
 end
 
+%% Indoor vs Outdoor
+load( [datapath,'Stim_Memorability_Exp1.mat'])
+
+% Separate conditions
+for s = 1:length(gaze2D)
+    disp(s)
+    %% load trial info to split conditions
+    
+    load([sacsavepath,sprintf('TrialEyeInfo_%d_%s.mat',s,'Exp1')])
+    
+    g_data = cat(3,gaze2D{s}{Eye_trlinfo(:,1)});
+Eye_trlinfo(:,9) = Img_id_enc{s}(Eye_trlinfo(:,1));
+    %% sep by In vs Outdoor
+    
+    Gaze_Indoor(:,:,s) = nanmean(g_data(:,:,Eye_trlinfo(:,9)<=162 & Eye_trlinfo(:,8)==1),3);
+    Gaze_outdoor(:,:,s) = nanmean(g_data(:,:,Eye_trlinfo(:,9)>162 & Eye_trlinfo(:,8)==1),3);
+    
+    
+    %% sep by In/ Outdoor x Memory with balancing N of trials 
+
+    [Gaze_InRem_balance(:,:,s),Gaze_InFor_balance(:,:,s),n_trl] = Gaze_balance_trial(g_data(:,:,Eye_trlinfo(:,2)==1 & Eye_trlinfo(:,8)==1 & Eye_trlinfo(:,9)<=162),g_data(:,:,Eye_trlinfo(:,2)==0 & Eye_trlinfo(:,8)==1& Eye_trlinfo(:,9)<=162),100);
+    n_trl_InRF_balance(s,1:2) = n_trl;
+    
+    [Gaze_OutRem_balance(:,:,s),Gaze_OutFor_balance(:,:,s),n_trl] = Gaze_balance_trial(g_data(:,:,Eye_trlinfo(:,2)==1 & Eye_trlinfo(:,8)==1 & Eye_trlinfo(:,9)>162),g_data(:,:,Eye_trlinfo(:,2)==0 & Eye_trlinfo(:,8)==1& Eye_trlinfo(:,9)>162),100);
+    n_trl_OutRF_balance(s,1:2) = n_trl;
+    
+   
+    
+    clearvars g_data cri
+end
+
+%%
+% In vs Outdoor
+subset = 1:20; % all subjects
+for hitvsmis = 1
+ave_2D_C1 = Gaze_Indoor;
+ave_2D_C2 = Gaze_outdoor;
+viewdva_x =28;
+viewdva_y =21;
+%%
+Dat_C1 = [];
+Dat_C1.dat = permute(ave_2D_C1,[4,1,2,3]);
+Dat_C1.freq = linspace(-viewdva_y/2,viewdva_y/2,size(ave_2D_C1,1))';
+Dat_C1.time = linspace(-viewdva_x/2,viewdva_x/2,size(ave_2D_C1,2))';
+Dat_C1.dimord = 'chan_freq_time_subj';
+Dat_C1.label = {'avg'};
+
+Dat_C2 = Dat_C1;
+Dat_C2.dat = permute(ave_2D_C2,[4,1,2,3]);
+
+
+design      = zeros(2,length(subset)*2);
+design(1,:) = repmat(1:length(subset),[1 2]);
+design(2,:) = [ones(1,length(subset)),ones(1,length(subset))+1];
+    
+cfg = [];
+cfg.spmversion = 'spm12';
+cfg.method           = 'montecarlo';
+cfg.statistic        = 'ft_statfun_depsamplesT';
+cfg.correctm         = 'cluster';
+cfg.clusteralpha     = 0.05;
+cfg.clusterstatistic = 'maxsum';
+cfg.ivar                = 2;
+cfg.uvar                = 1;
+cfg.parameter           = 'dat';
+cfg.tail             = 0;
+cfg.clustertail      = 0;
+cfg.alpha            = 0.025;
+cfg.numrandomization = 1000;
+
+cfg.design           = design;
+
+
+[stat_CB]  = ft_freqstatistics(cfg, Dat_C1,Dat_C2);
+
+end
+
+
+
+% In Rem vs Forg
+subset = 1:20; % all subjects
+for hitvsmis = 1
+ave_2D_C1 = Gaze_InRem_balance;
+ave_2D_C2 = Gaze_InFor_balance;
+viewdva_x =28;
+viewdva_y =21;
+%%
+Dat_C1 = [];
+Dat_C1.dat = permute(ave_2D_C1,[4,1,2,3]);
+Dat_C1.freq = linspace(-viewdva_y/2,viewdva_y/2,size(ave_2D_C1,1))';
+Dat_C1.time = linspace(-viewdva_x/2,viewdva_x/2,size(ave_2D_C1,2))';
+Dat_C1.dimord = 'chan_freq_time_subj';
+Dat_C1.label = {'avg'};
+
+Dat_C2 = Dat_C1;
+Dat_C2.dat = permute(ave_2D_C2,[4,1,2,3]);
+
+
+design      = zeros(2,length(subset)*2);
+design(1,:) = repmat(1:length(subset),[1 2]);
+design(2,:) = [ones(1,length(subset)),ones(1,length(subset))+1];
+    
+cfg = [];
+cfg.spmversion = 'spm12';
+cfg.method           = 'montecarlo';
+cfg.statistic        = 'ft_statfun_depsamplesT';
+cfg.correctm         = 'cluster';
+cfg.clusteralpha     = 0.05;
+cfg.clusterstatistic = 'maxsum';
+cfg.ivar                = 2;
+cfg.uvar                = 1;
+cfg.parameter           = 'dat';
+cfg.tail             = 0;
+cfg.clustertail      = 0;
+cfg.alpha            = 0.025;
+cfg.numrandomization = 1000;
+
+cfg.design           = design;
+
+
+[stat_CB_In]  = ft_freqstatistics(cfg, Dat_C1,Dat_C2);
+
+end
+
+
+% Out Rem vs Forg
+subset = 1:20; % all subjects
+for hitvsmis = 1
+ave_2D_C1 = Gaze_OutRem_balance;
+ave_2D_C2 = Gaze_OutFor_balance;
+viewdva_x =28;
+viewdva_y =21;
+%%
+Dat_C1 = [];
+Dat_C1.dat = permute(ave_2D_C1,[4,1,2,3]);
+Dat_C1.freq = linspace(-viewdva_y/2,viewdva_y/2,size(ave_2D_C1,1))';
+Dat_C1.time = linspace(-viewdva_x/2,viewdva_x/2,size(ave_2D_C1,2))';
+Dat_C1.dimord = 'chan_freq_time_subj';
+Dat_C1.label = {'avg'};
+
+Dat_C2 = Dat_C1;
+Dat_C2.dat = permute(ave_2D_C2,[4,1,2,3]);
+
+
+design      = zeros(2,length(subset)*2);
+design(1,:) = repmat(1:length(subset),[1 2]);
+design(2,:) = [ones(1,length(subset)),ones(1,length(subset))+1];
+    
+cfg = [];
+cfg.spmversion = 'spm12';
+cfg.method           = 'montecarlo';
+cfg.statistic        = 'ft_statfun_depsamplesT';
+cfg.correctm         = 'cluster';
+cfg.clusteralpha     = 0.05;
+cfg.clusterstatistic = 'maxsum';
+cfg.ivar                = 2;
+cfg.uvar                = 1;
+cfg.parameter           = 'dat';
+cfg.tail             = 0;
+cfg.clustertail      = 0;
+cfg.alpha            = 0.025;
+cfg.numrandomization = 1000;
+
+cfg.design           = design;
+
+
+[stat_CB_Out]  = ft_freqstatistics(cfg, Dat_C1,Dat_C2);
+
+end
+
+
+%% save output for plotting
+
+save([datafigspath,'SuppleFig2_2.mat'],'stat_CB*','Gaze_Indoor','Gaze_outdoor','Gaze_OutRem_balance','Gaze_OutFor_balance','Gaze_InRem_balance','Gaze_InFor_balance')
+%% Figure
+codepath = '';
+datafigspath =  [codepath,'\data4figs\'];
+figsavepath = [codepath,'\figures\'];
+% load behavioral data, gaze data
+load( [datafigspath,'SuppleFig2_2.mat'])
+
+plot_save = 1; % to save or not figure
+
+sizefig1 = [0,0.2,0.3,0.45];
+fontaxis = 24;line_width = 2;
+alpha = 0.05;n_bin_x = 80;n_bin_y = 60;
+viewdva_x =28;
+viewdva_y =21;
+
+fig_savename1 = 'SuppleFig2A_bottomright';
+for hitvsmis=1
+f=figure('Name',int2str(11),'units','normalized','outerposition',sizefig1);clf;
+Cluster=zeros(n_bin_y,n_bin_x);
+Cluster(squeeze(stat_CB.mask)) = 1;
+[B,L,N,A] = bwboundaries(Cluster); 
+imagesc(squeeze(stat_CB.stat));hold on
+for k=1:length(B)
+     boundary = B{k};
+    plot(boundary(:,2), boundary(:,1), 'LineWidth',3,'color','k');
+end
+hold off
+xticks(linspace(0,n_bin_x,5))
+xticklabels(num2cell(abs(linspace(0,n_bin_x,5)*viewdva_x/n_bin_x-viewdva_x/2)))
+yticks(linspace(0,n_bin_y,5))
+yticklabels(num2cell(abs(linspace(0,n_bin_y,5)*viewdva_y/n_bin_y-viewdva_y/2)))
+caxis([-2.5,2.5])
+xlabel('Dist to center [dva]')
+%ylabel('Dist to center [dva]')
+colormap(flipud(cbrewer2('RdBu','div')));
+c = colorbar;
+c.Label.String = 't value';
+title('Gaze density Indoor vs Outdoor')
+xlabel('Horizontal [dva]' )
+ylabel('Vertical [dva]' )
+set(findobj(gcf,'type','axes'),'FontName','Arial','FontSize',fontaxis,'FontWeight','normal', 'LineWidth', line_width);
+
+    if plot_save
+        saveas(gcf,[figsavepath,fig_savename1,'.emf'])
+        saveas(gcf,[figsavepath,fig_savename1,'.png'])
+    end
+    
+end 
+
+fig_savename1 = 'SuppleFig2B_topright';
+for hitvsmis=1
+f=figure('Name',int2str(11),'units','normalized','outerposition',sizefig1);clf;
+Cluster=zeros(n_bin_y,n_bin_x);
+Cluster(squeeze(stat_CB_In.mask)) = 1;
+[B,L,N,A] = bwboundaries(Cluster); 
+imagesc(squeeze(stat_CB_In.stat));hold on
+for k=1:length(B)
+     boundary = B{k};
+    plot(boundary(:,2), boundary(:,1), 'LineWidth',3,'color','k');
+end
+hold off
+xticks(linspace(0,n_bin_x,5))
+xticklabels(num2cell(abs(linspace(0,n_bin_x,5)*viewdva_x/n_bin_x-viewdva_x/2)))
+yticks(linspace(0,n_bin_y,5))
+yticklabels(num2cell(abs(linspace(0,n_bin_y,5)*viewdva_y/n_bin_y-viewdva_y/2)))
+caxis([-2.5,2.5])
+xlabel('Dist to center [dva]')
+%ylabel('Dist to center [dva]')
+colormap(flipud(cbrewer2('RdBu','div')));
+c = colorbar;
+c.Label.String = 't value';
+title('Gaze density Indoor: Rem vs Forg')
+xlabel('Horizontal [dva]' )
+ylabel('Vertical [dva]' )
+set(findobj(gcf,'type','axes'),'FontName','Arial','FontSize',fontaxis,'FontWeight','normal', 'LineWidth', line_width);
+
+    if plot_save
+        saveas(gcf,[figsavepath,fig_savename1,'.emf'])
+        saveas(gcf,[figsavepath,fig_savename1,'.png'])
+    end
+    
+end 
+
+fig_savename1 = 'SuppleFig2B_bottomright';
+for hitvsmis=1
+f=figure('Name',int2str(11),'units','normalized','outerposition',sizefig1);clf;
+Cluster=zeros(n_bin_y,n_bin_x);
+Cluster(squeeze(stat_CB_Out.mask)) = 1;
+[B,L,N,A] = bwboundaries(Cluster); 
+imagesc(squeeze(stat_CB_Out.stat));hold on
+for k=1:length(B)
+     boundary = B{k};
+    plot(boundary(:,2), boundary(:,1), 'LineWidth',3,'color','k');
+end
+hold off
+xticks(linspace(0,n_bin_x,5))
+xticklabels(num2cell(abs(linspace(0,n_bin_x,5)*viewdva_x/n_bin_x-viewdva_x/2)))
+yticks(linspace(0,n_bin_y,5))
+yticklabels(num2cell(abs(linspace(0,n_bin_y,5)*viewdva_y/n_bin_y-viewdva_y/2)))
+caxis([-2.5,2.5])
+xlabel('Dist to center [dva]')
+%ylabel('Dist to center [dva]')
+colormap(flipud(cbrewer2('RdBu','div')));
+c = colorbar;
+c.Label.String = 't value';
+title('Gaze density Outdoor: Rem vs Forg')
+xlabel('Horizontal [dva]' )
+ylabel('Vertical [dva]' )
+set(findobj(gcf,'type','axes'),'FontName','Arial','FontSize',fontaxis,'FontWeight','normal', 'LineWidth', line_width);
+
+    if plot_save
+        saveas(gcf,[figsavepath,fig_savename1,'.emf'])
+        saveas(gcf,[figsavepath,fig_savename1,'.png'])
+    end
+    
+end 
--- Gaze/GazeAnalysis_Exp3.m
+++ Gaze/GazeAnalysis_Exp3.m
@@ -29,6 +29,12 @@ for s = 1:length(gaze2D)
     
     Gaze_MoreExplr(:,:,s) = nanmean(g_data(:,:,Eye_trlinfo(:,7)<=cri & Eye_trlinfo(:,8)==1),3);
     Gaze_LessExplr(:,:,s) = nanmean(g_data(:,:,Eye_trlinfo(:,7)>=cri & Eye_trlinfo(:,8)==1),3);
+    
+    %% sep by Saccade count
+    cri = nanmedian(Eye_trlinfo(Eye_trlinfo(:,8)==1,5));
+    
+    Gaze_MoreSac(:,:,s) = nanmean(g_data(:,:,Eye_trlinfo(:,5)>=cri & Eye_trlinfo(:,8)==1),3);
+    Gaze_FewerSac(:,:,s) = nanmean(g_data(:,:,Eye_trlinfo(:,5)<=cri & Eye_trlinfo(:,8)==1),3);
  
     clearvars g_data cri
     
@@ -123,11 +129,58 @@ cfg.design           = design;
 
 end
 
+
+% More vs Fewer Saccades
+for MvLSac = 1
+ave_2D_C1 = Gaze_MoreSac;
+ave_2D_C2 = Gaze_FewerSac;
+viewdva_x =28;
+viewdva_y =21;
+%%
+Dat_C1 = [];
+Dat_C1.dat = permute(ave_2D_C1,[4,1,2,3]);
+Dat_C1.freq = linspace(-viewdva_y/2,viewdva_y/2,size(ave_2D_C1,1))';
+Dat_C1.time = linspace(-viewdva_x/2,viewdva_x/2,size(ave_2D_C1,2))';
+Dat_C1.dimord = 'chan_freq_time_subj';
+Dat_C1.label = {'avg'};
+
+Dat_C2 = Dat_C1;
+Dat_C2.dat = permute(ave_2D_C2,[4,1,2,3]);
+
+
+design      = zeros(2,length(subset)*2);
+design(1,:) = repmat(1:length(subset),[1 2]);
+design(2,:) = [ones(1,length(subset)),ones(1,length(subset))+1];
+    
+cfg = [];
+cfg.spmversion = 'spm12';
+cfg.method           = 'montecarlo';
+cfg.statistic        = 'ft_statfun_depsamplesT';
+cfg.correctm         = 'cluster';
+cfg.clusteralpha     = 0.05;
+cfg.clusterstatistic = 'maxsum';
+cfg.ivar                = 2;
+cfg.uvar                = 1;
+cfg.parameter           = 'dat';
+cfg.tail             = 0;
+cfg.clustertail      = 0;
+cfg.alpha            = 0.025;
+cfg.numrandomization = 1000;
+
+cfg.design           = design;
+
+
+[stat_Sac]  = ft_freqstatistics(cfg, Dat_C1,Dat_C2);
+
+end
+
+
 %%
 %% save output for plotting
 save([datafigspath,'Fig3B_right.mat'],'stat_CB')
 save([datafigspath,'Fig3D_bottom.mat'],'stat_EI')
-save([datafigspath,'SuppleFig4_right31.mat'],'stat_EI','Gaze_MoreExplr','Gaze_LessExplr')
+save([datafigspath,'SuppleFig9_right31.mat'],'stat_EI','Gaze_MoreExplr','Gaze_LessExplr')
+save([datafigspath,'SuppleFig10A_left.mat'],'stat_Sac')
 %% Figure
 % load data for plotting
 codepath = '';
@@ -193,6 +246,54 @@ Cluster(squeeze(stat_EI.mask)) = 1;
 imAlpha = ones(size(squeeze(stat_EI.stat)));
 imAlpha(isnan(squeeze(stat_EI.stat))) = 0;% replace the NaN with 0 for visualization
 imagesc(squeeze(stat_EI.stat),'AlphaData',imAlpha);
+hold on
+for k=1:length(B)
+     boundary = B{k};
+    plot(boundary(:,2), boundary(:,1), 'LineWidth',3,'color','k');
+end
+
+hold off
+xticks(linspace(0,n_bin_x,5))
+xticklabels(num2cell(abs(linspace(0,n_bin_x,5)*viewdva_x/n_bin_x-viewdva_x/2)))
+yticks(linspace(0,n_bin_y,5))
+yticklabels(num2cell(abs(linspace(0,n_bin_y,5)*viewdva_y/n_bin_y-viewdva_y/2)))
+caxis([-2.5,2.5])
+xlabel('Dist to center [dva]')
+%ylabel('Dist to center [dva]')
+colormap(flipud(cbrewer2('RdBu','div')));
+c = colorbar;
+c.Label.String = 't value';
+set(findobj(gcf,'type','axes'),'FontName','Arial','FontSize',fontaxis,'FontWeight','normal', 'LineWidth', line_width);
+
+    if plot_save
+        saveas(gcf,[figsavepath,fig_savename1,'.emf'])
+        saveas(gcf,[figsavepath,fig_savename1,'.png'])
+    end
+    
+end 
+
+%%
+load([datafigspath,'SuppleFig10A_left.mat'])
+
+plot_save = 1; % to save or not figure
+
+sizefig1 = [0,0.2,0.3,0.45];
+fontaxis = 24;line_width = 2;
+alpha = 0.05;n_bin_x = 80;n_bin_y = 60;
+viewdva_x =28;
+viewdva_y =21;
+
+%
+fig_savename1 = 'SuppleFig10A_left';
+
+for MvLSac=1
+f=figure('Name',int2str(11),'units','normalized','outerposition',sizefig1);clf;
+Cluster=zeros(n_bin_y,n_bin_x);
+Cluster(squeeze(stat_Sac.mask)) = 1;
+[B,L,N,A] = bwboundaries(Cluster); 
+imAlpha = ones(size(squeeze(stat_Sac.stat)));
+imAlpha(isnan(squeeze(stat_Sac.stat))) = 0;% replace the NaN with 0 for visualization
+imagesc(squeeze(stat_Sac.stat),'AlphaData',imAlpha);
 hold on
 for k=1:length(B)
      boundary = B{k};
